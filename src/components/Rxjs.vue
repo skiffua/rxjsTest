372fe52d4a762d6a134dfdb441fec8f49fb9977f
Oleh <oleh_mushora@ukr.net>
2020-02-12 11:20:57 +0200

    add base configuration, first page

---
+++ src/components/Rxjs.vue
@@ -0,0 +1,32 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+    import { Observable } from "rxjs/Observable";
+    import "rxjs/add/observable/from";
+
+    export default {
+        name: "Rxjs",
+
+        created() {
+            let numbers = [1, 5, 10];
+            let source = Observable.from(numbers);
+            // eslint-disable-next-line no-console
+            console.log(source)
+
+
+
+        },
+
+        methods: {
+            test() {
+
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
